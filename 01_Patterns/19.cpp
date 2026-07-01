7e91a0597c7faec82da9f1d194cafe830a8c5254
Ashwin-here01 <ashwin.online01@gmail.com>
2026-07-01 14:08:36 +0530

    Did pattern 19

---
+++ 01_Patterns/19.cpp
@@ -0,0 +1,58 @@
+/*
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+*/
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void pattern19(int n) {
+        for(int i = n; i > 0; i--) {
+            for(int j = 0; j < i; j++) {
+                cout << "*";
+            }
+            for(int j = 0; j < (n-i)*2; j++) {
+                cout << " ";
+            }
+            for(int j = 0; j < i; j++) {
+                cout << "*";
+            }
+            cout << endl;
+        }
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                cout << "*";
+            }
+            for(int j = 0; j < (n-i)*2; j++) {
+                cout << " ";
+            }
+            for(int j = 0; j < i; j++) {
+                cout << "*";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    Solution obj;
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    obj.pattern19(n);
+
+    return 0;
+}
